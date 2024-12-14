21433a82cc8d9e5838d6001f73246923254349e6
irakeshbind <rakeshbind351998@gmail.com>
2024-12-14 17:49:53 +0530

    project create

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './component/style.css'
+import Signup from './component/Signup'
+function App() {
+  return (
+    <div>
+     <Signup/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/Signup.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Signup() {
+  return (
+    <div className='signup-wrapper'>
+        <div className='signup-box'>
+             <div className='signup-left-box'>
+              
+             </div> 
+             <div className='signup-rigth-box'>
+              
+             </div>
+        </div>
+    </div>
+  )
+}
+
+export default Signup
