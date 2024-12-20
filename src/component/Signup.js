f7e433b1e4d23c563983937a2ab5074bef043efa
irakeshbind <rakeshbind351998@gmail.com>
2024-12-20 11:22:21 +0530

    lms project

--- src/component/Signup.js
+++ src/component/Signup.js
@@ -4,12 +4,15 @@ function Signup() {
   return (
     <div className='signup-wrapper'>
         <div className='signup-box'>
-             <div className='signup-left-box'>
-              
-             </div> 
-             <div className='signup-rigth-box'>
-              
-             </div>
+            <div className='signup-left-box'>
+                 <img alt='books' src={require('../component/assets/yup.png')}/>
+                 <h1>SBS  Online Classes</h1>
+                 <p>Learn Coding in Easy way ...</p>
+            </div>
+            <div className='signup-right-box'>
+
+            </div>
+
         </div>
     </div>
   )
