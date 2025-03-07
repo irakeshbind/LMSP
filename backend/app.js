70322ff4e93cc18ed70fb3006e08eb1d975b3a15
irakeshbind <rakeshbind351998@gmail.com>
2025-03-07 00:47:53 -0600

    create app

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.get('/signup',(req,res)=>{
+//     res.status(200).json({
+//         msg:'signup request'
+
+// })
+console.log('hello')
+})
 
 
 module.exports = app;
