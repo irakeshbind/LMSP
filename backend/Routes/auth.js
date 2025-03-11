b96351cf0d14f6f873e83f616fa5b49fa1eda2f4
irakeshbind <rakeshbind351998@gmail.com>
2025-03-11 00:33:27 -0500

    signup api create

--- backend/Routes/auth.js
+++ backend/Routes/auth.js
@@ -1,13 +1,62 @@
 const express = require('express')
 const router = express.Router();
+const User = require('../Models/User')
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+// api admin signup
 
-router.post('/signup',(req,res)=>{
+// api admin login
+router.post('/admin/sinup',async(req,res)=>{
+  try{
+    const users = await User.find({email:req.body.email})
+    if(users.length>0)
+    {
+        return res.status(500).json({
+            error:'email already registered'
+        })
+    }
+    const hashCode = await bcrypt.hash(req.body.password,10)
+    const user = new User({
+        _id:new mongoose.Types.ObjectId,
+        fullName:req.body.fullName,
+        email:req.body.email,
+        phone:req.body.phone,
+        password:hashCode
+    })
+    const data = await user.save()
+    res.status(200).json({
+        newuser:data
+    })
+  }
+  catch(err)
+  {
+    console.log(err)
+    res.status(500).json({
+        error:err
+    })
+  }
+})
+
+router.post('/admin/login', (req, res) => {
     console.log(req.files)
     res.status(200).json({
-        msg:'mmm'
+        msg: 'login admin'
     })
 })
 
+router.post('/user/signup', (req, res) => {
+    console.log(req.files)
+    res.status(200).json({
+        msg: 'login signup'
+    })
+})
+
+router.post('/user/login', (req, res) => {
+    console.log(req.files)
+    res.status(200).json({
+        msg: 'login user'
+    })
+})
 
 
 module.exports = router;
